e7b49bdb451f086d6968bd437333027325cee899
a.isakov <a.isakov@skbkontur.ru>
2020-12-29 21:04:35 +0500

    [new] UI

--- app/src/main/java/com/example/myguides/LoginActivity.kt
+++ app/src/main/java/com/example/myguides/LoginActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +21,11 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_login)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
--- app/src/main/java/com/example/myguides/MenuActivity.kt
+++ app/src/main/java/com/example/myguides/MenuActivity.kt
@@ -4,11 +4,19 @@ import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
+
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
         setContentView(R.layout.activity_menu)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     }
